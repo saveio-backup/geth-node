7e99192546ea7d7aede50c15466991d7121eecd5
smallyunet <dev.wangyu@gmail.com>
2022-08-04 17:09:49 +0800

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 
-geth --datadir dev-chain --dev --http --http.api personal,web3,eth,net --http.corsdomain "remix.ethereum.org" --ws --ws.origins="*" --password password.txt
+geth \
+--datadir dev-chain \
+--dev --dev.gaslimit '9000000000000' \
+--http --http.api personal,web3,eth,net --http.corsdomain "remix.ethereum.org" \
+--ws --ws.origins="*" \
+--password password.txt \
+--mine --miner.gaslimit '9000000000000' \
+
 
